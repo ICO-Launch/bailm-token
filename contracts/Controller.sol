db083a86e1a989f7d9ed9e444575ca52025bb064
manuel <manologr89@gmail.com>
2018-09-21 12:34:04 +0200

    Fix package for RBAC availability

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,9 +1,9 @@
 
 pragma solidity ^0.4.23;
 
-import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
-import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
-import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 
 contract Controller is MintableToken, PausableToken {
   address public thisAddr; // matches delegation slot in proxy
